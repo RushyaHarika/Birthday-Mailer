91b871a1735f94182ec04acd041175c6074ae08d
RushyaHarika <40599543+RushyaHarika@users.noreply.github.com>
2020-05-23 14:18:55 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var moment = require('moment');
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('birthday');   //render() looks for a file in views
+});
+
+
+router.post('/postbirthday', function(req,res){
+  var bdate = moment(req.body.dob).format('DD-MM');
+  console.log(bdate);
+  // var Time = moment().format('hh:mm:ss:a');
+  // console.log(Time);
+  var Date = moment().format('DD-MM');
+  console.log(Date);
+  if(bdate==Date){
+    var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'akash.talluri.77@gmail.com',
+      pass: 'Akash.Talluri.77'
+    }
+    });
+
+    var mailOptions = {
+      from: 'Dear Friend',
+      to: req.body.email,
+      subject: 'Birthday Wishes',
+      text: 'Hi ' + req.body.name+ ' Happy Birthday!!!'+'\n'+req.body.message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent');
+        res.send(info);
+      }
+    });
+  }
+});
+
+
+module.exports = router;
